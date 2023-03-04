f450a23388e8d53361935eadd31a427fa77685e2
John <john72831@hotmail.com>
2023-03-05 03:12:29 +0800

    Add John.ImplicitAndExplicitOperator

---
+++ John.ImplicitAndExplicitOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿var guid = Guid.NewGuid();
+//By explicit operator
+UserId userId = (UserId)guid;
+//By implicit operator
+guid = userId;
+
+class UserId
+{
+    public Guid Id { get; set; }
+
+    public UserId(Guid id)
+    {
+        Id = id;
+    }
+
+    /// <summary>
+    /// 明確轉換
+    /// </summary>
+    public static explicit operator UserId(Guid id)
+    {
+        return new UserId(id);
+    }
+
+    /// <summary>
+    /// 隱含轉換
+    /// </summary>
+    public static implicit operator Guid(UserId userId)
+    {
+        return userId.Id;
+    }
+}
