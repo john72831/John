1d9941c19ddf2588506b71fd839ba5adf4fdec77
John <john72831@hotmail.com>
2023-03-06 22:04:56 +0800

    Add John.PriorityQueue

---
+++ John.PriorityQueue/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+var source = new CancellationTokenSource();
+var queue = new PriorityQueue<string, (Status, long)>(StatusCompare.Instance);
+
+queue.Enqueue("Mike", (Status.Normal, Stopwatch.GetTimestamp()));
+queue.Enqueue("Nike", (Status.Gold, Stopwatch.GetTimestamp()));
+queue.Enqueue("John", (Status.Gold, Stopwatch.GetTimestamp()));
+queue.Enqueue("Poke", (Status.Normal, Stopwatch.GetTimestamp()));
+
+
+while (!source.IsCancellationRequested)
+{
+    if (queue.Count > 0)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine(queue.Dequeue());
+    }
+}
+
+enum Status
+{
+    Normal,
+    Gold,
+    Platinum
+}
+
+class StatusCompare : IComparer<(Status, long)>
+{
+    public static readonly StatusCompare Instance = new();
+
+    int IComparer<(Status, long)>.Compare((Status, long) x, (Status, long) y)
+    {
+        if (x.Item1 == y.Item1)
+            return x.CompareTo(y);
+
+        return y.CompareTo(x);
+    }
+}
