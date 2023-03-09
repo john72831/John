6be70db29ce31d3e2b00ca464cf1e408d6abde5f
John <john72831@hotmail.com>
2023-03-10 01:25:24 +0800

    Add John.John.Humanizer

---
+++ John.Humanizer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Humanizer;
+using System.ComponentModel;
+using System.Globalization;
+
+var traditionalChinese = new CultureInfo("zh-Hant");
+
+var strings = new Func<string>[]
+{
+    //General
+    () => "This is the story of my life".Truncate(14, "..."),
+    () => ExampleEnum.FirstValue.Humanize(),
+
+    //DateTime
+    () => DateTime.UtcNow.AddHours(2).Humanize(),
+
+    //TimeSpans
+    () => TimeSpan.FromMilliseconds(252000).Humanize(1),
+
+    //Case
+    () => "John Lin".Pascalize(),
+
+    //Culture
+    () => 1.ToOrdinalWords(traditionalChinese),
+    () => 69.Millions().ToString()
+};
+
+foreach (var textFunc in strings)
+{
+    Console.WriteLine(textFunc());
+}
+
+enum ExampleEnum
+{
+    [Description("First Value")]
+    FirstValue,
+    SecondValue
+}
