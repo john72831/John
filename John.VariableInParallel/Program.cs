2e0b3b4797c23990980db0a75f9893d95dc4f068
John <john72831@hotmail.com>
2023-03-15 23:51:29 +0800

    Add John.VariableInParallel

---
+++ John.VariableInParallel/Program.cs
@@ -0,0 +1,58 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchy>();
+
+[MemoryDiagnoser(false)]
+public class Benchy
+{
+    [Params(10000, 10000000)]
+    public int Range { get; set; }
+
+    [Params(2,4,8)]
+    public int MaxDegreeOfParallelism { get; set; }
+
+    [Benchmark]
+    public void For()
+    {
+        var numbers = Enumerable.Range(1, Range);
+        long sumOfNumbers = 0;
+        Action<long> taskFinishedMethod = (taskResult) =>
+        {
+            Interlocked.Add(ref sumOfNumbers, taskResult);
+        };
+
+        Parallel.For(
+            0,
+            numbers.Count() + 1,
+            new ParallelOptions() { MaxDegreeOfParallelism = MaxDegreeOfParallelism },
+            () => 0, // initialize the thread local variable
+            (j, parallelLoopState, subtotal) =>
+            {
+                subtotal += j;
+                return subtotal;
+            }, // method invoked by the loop on each iteration
+            taskFinishedMethod
+        );
+    }
+
+    [Benchmark]
+    public void Foreach()
+    {
+        var numbers = Enumerable.Range(1, Range);
+        long sumOfNumbers = 0;
+        Action<long> taskFinishedMethod = (taskResult) =>
+        {
+            Interlocked.Add(ref sumOfNumbers, taskResult);
+        };
+
+        Parallel.ForEach(
+            numbers,
+            new ParallelOptions() { MaxDegreeOfParallelism = MaxDegreeOfParallelism },
+            () => 0, (j, parallelLoopState, subtotal) =>
+            {
+                subtotal += j;
+                return subtotal;
+            }, taskFinishedMethod);
+    }
+}
